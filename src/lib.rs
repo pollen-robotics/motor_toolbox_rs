1ad9a30d307dec718680a18905bf9c9f17088f5c
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-08-17 20:53:43 +0200

    Add multiple motor controller trait.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ mod coherency;
 mod motor_controller;
 pub use motor_controller::{MotorController, Result};
 
+mod multiple_motors_controller;
+pub use multiple_motors_controller::{MultipleMotorsController, MultipleMotorsControllerWrapper};
