dbab4edc1817ccd519943df43b20eaeea397a4fd
Steve Nguyen <steve.nguyen@pollen-robotics.com>
2024-12-02 17:59:56 +0100

    default

--- src/motors_io.rs
+++ src/motors_io.rs
@@ -20,21 +20,33 @@ pub trait RawMotorsIO<const N: usize> {
     fn set_target_position(&mut self, position: [f64; N]) -> Result<()>;
 
     /// Get the current target torque of the motors (in Nm)
-    fn get_target_torque(&mut self) -> Result<[f64; N]>;
+    fn get_target_torque(&mut self) -> Result<[f64; N]> {
+        Err("get_target_torque not implemented".into())
+    }
     /// Set the current target torque of the motors (in Nm)
-    fn set_target_torque(&mut self, torque: [f64; N]) -> Result<()>;
+    fn set_target_torque(&mut self, torque: [f64; N]) -> Result<()> {
+        Err("set_target_torque not implemented".into())
+    }
 
     /// Set the current target velocity of the motors (in rad/s)
-    fn set_target_velocity(&mut self, velocity: [f64; N]) -> Result<()>;
+    fn set_target_velocity(&mut self, velocity: [f64; N]) -> Result<()> {
+        Err("set_target_velocity not implemented".into())
+    }
 
     /// Get the current target velocity of the motors (in rad/s)
-    fn get_target_velocity(&mut self) -> Result<[f64; N]>;
+    fn get_target_velocity(&mut self) -> Result<[f64; N]> {
+        Err("get_target_velocity not implemented".into())
+    }
 
     /// Set the control mode
-    fn set_control_mode(&mut self, mode: [u8; N]) -> Result<()>;
+    fn set_control_mode(&mut self, mode: [u8; N]) -> Result<()> {
+        Err("set_control_mode not implemented".into())
+    }
 
     /// Get the control mode
-    fn get_control_mode(&mut self) -> Result<[u8; N]>;
+    fn get_control_mode(&mut self) -> Result<[u8; N]> {
+        Err("get_control_mode not implemented".into())
+    }
 
     /// Set the current target position and returns the motor feeback (position, velocity, torque)
     fn set_target_position_fb(&mut self, position: [f64; N]) -> Result<[f64; N]>;
